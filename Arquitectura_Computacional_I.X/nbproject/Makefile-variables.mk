d2cc7380261608fdb5af4b6d205f0b38f85f2ce3
Josué <josue.garbanzol@gmail.com>
2026-06-02 02:44:48 -0600

    cambio de nombre del repositorio

---
+++ Arquitectura_Computacional_I.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Arqui.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Arqui.X.production.hex
+# mathew configuration
+CND_ARTIFACT_DIR_mathew=dist/mathew/production
+CND_ARTIFACT_NAME_mathew=Arqui.X.production.hex
+CND_ARTIFACT_PATH_mathew=dist/mathew/production/Arqui.X.production.hex
