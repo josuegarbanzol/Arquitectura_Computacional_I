42e2cb37c16b328e2e57b3e0a2a3a1ca4e25cf82
Josué <josue.garbanzol@gmail.com>
2026-06-02 03:02:37 -0600

    Se inicio la programacion del los Leds

--- Arquitectura_Computacional_I.X/nbproject/Makefile-impl.mk
+++ Arquitectura_Computacional_I.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Arqui.X
+PROJECTNAME=Arquitectura_Computacional_I.X
 
 # Active Configuration
 DEFAULTCONF=default
--- Arquitectura_Computacional_I.X/nbproject/Makefile-variables.mk
+++ Arquitectura_Computacional_I.X/nbproject/Makefile-variables.mk
@@ -6,9 +6,9 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Arqui.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Arqui.X.production.hex
+CND_ARTIFACT_NAME_default=Arquitectura_Computacional_I.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Arquitectura_Computacional_I.X.production.hex
 # mathew configuration
 CND_ARTIFACT_DIR_mathew=dist/mathew/production
-CND_ARTIFACT_NAME_mathew=Arqui.X.production.hex
-CND_ARTIFACT_PATH_mathew=dist/mathew/production/Arqui.X.production.hex
+CND_ARTIFACT_NAME_mathew=Arquitectura_Computacional_I.X.production.hex
+CND_ARTIFACT_PATH_mathew=dist/mathew/production/Arquitectura_Computacional_I.X.production.hex
